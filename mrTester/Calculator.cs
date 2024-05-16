68cae0972e6b7c02e17ee64cb5f9600760e223a8
mthunzi gcwensa <mthunzigcwensa659@gmail.com>
2024-05-16 13:46:56 +0200

    Add project files.

---
+++ mrTester/Calculator.cs
@@ -0,0 +1,11 @@
+﻿namespace mrTester
+{
+	public class Calculator
+	{
+		public int AddNumbers(int a, int b)
+		{
+			return a + b;
+		}
+
+	}
+}
---
+++ mrtestMSTest/CalculatorMSTest.cs
@@ -0,0 +1,27 @@
+﻿using mrTester;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mrtestMSTest
+{
+	[TestClass]
+	public class CalculatorMSTest
+	{
+		[TestMethod]
+		public void AddNumbers_InputTwoInt_GetCorrectAddition()
+		{
+			//Arrange
+			Calculator calc = new();
+
+			//Act
+			int result = calc.AddNumbers(10, 20);
+
+			//Assert 
+			Assert.AreEqual(30, result);
+
+		}
+	}
+}
